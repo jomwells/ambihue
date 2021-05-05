7e88bcb9ed812e37f8ad0094673f2cb8cef7c3a2
Mr-Groch <grocholski@gmail.com>
2021-05-05 10:49:36 +0200

    0.0.3 - configurable secured_transport and api_version

--- custom_components/philips_ambilight+hue/switch.py
+++ custom_components/philips_ambilight+hue/switch.py
@@ -6,16 +6,18 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.components.switch import (
     DOMAIN, PLATFORM_SCHEMA, SwitchEntity, ENTITY_ID_FORMAT)
-from homeassistant.const import (CONF_HOST, CONF_NAME, CONF_USERNAME, CONF_PASSWORD, CONF_ID, STATE_OFF, STATE_STANDBY, STATE_ON)
+from homeassistant.const import (CONF_HOST, CONF_API_VERSION, CONF_NAME, CONF_USERNAME, CONF_PASSWORD, CONF_ID, STATE_OFF, STATE_STANDBY, STATE_ON)
 from requests.auth import HTTPDigestAuth
 from requests.adapters import HTTPAdapter
 
 DEFAULT_DEVICE = 'default'
 DEFAULT_HOST = '127.0.0.1'
+DEFAULT_API_VERSION = 6
+DEFAULT_SECURED_TRANSPORT = True
 DEFAULT_USER = 'user'
 DEFAULT_PASS = 'pass'
 DEFAULT_NAME = 'Ambilight+Hue'
-BASE_URL = 'http://{0}:1925/6/{1}' # for older philps tv's, try changing this to 'http://{0}:1925/1/{1}'
+BASE_URL = '{0}://{1}:{2}/{3}/{4}'
 TIMEOUT = 5.0
 CONNFAILCOUNT = 5
 
@@ -23,6 +25,8 @@ CONNFAILCOUNT = 5
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Required(CONF_API_VERSION, default=DEFAULT_API_VERSION): vol.In([1, 5, 6]),
+    vol.Required('secured_transport', default=DEFAULT_SECURED_TRANSPORT): cv.boolean,
     vol.Required(CONF_USERNAME, default=DEFAULT_USER): cv.string,
     vol.Required(CONF_PASSWORD, default=DEFAULT_PASS): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -31,15 +35,24 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
+    api_version = config.get(CONF_API_VERSION)
+    secured_transport = config.get('secured_transport')
     user = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
-    add_devices([AmbiHue(name, host, user, password)])
+    add_devices([AmbiHue(name, host, api_version, secured_transport, user, password)])
 
 class AmbiHue(SwitchEntity):
 
-    def __init__(self, name, host, user, password):
+    def __init__(self, name, host, api_version, secured_transport, user, password):
         self._name = name
         self._host = host
+        self._api_version = api_version
+        if secured_transport:
+            self._protocol = "https"
+            self._port = 1926
+        else:
+            self._protocol = "http"
+            self._port = 1925
         self._user = user
         self._password = password
         self._state = False
@@ -95,7 +108,7 @@ class AmbiHue(SwitchEntity):
             if self._connfail:
                 self._connfail -= 1
                 return False
-            resp = self._session.get(BASE_URL.format(self._host, path), verify=False, auth=HTTPDigestAuth(self._user, self._password), timeout=TIMEOUT)
+            resp = self._session.get(BASE_URL.format(self._protocol, self._host, self._port, self._api_version, path), verify=False, auth=HTTPDigestAuth(self._user, self._password), timeout=TIMEOUT)
             self.on = True
             return json.loads(resp.text)
         except requests.exceptions.RequestException as err:
@@ -108,7 +121,7 @@ class AmbiHue(SwitchEntity):
             if self._connfail:
                 self._connfail -= 1
                 return False
-            resp = self._session.post(BASE_URL.format(self._host, path), data=json.dumps(data), verify=False, auth=HTTPDigestAuth(self._user, self._password), timeout=TIMEOUT)
+            resp = self._session.post(BASE_URL.format(self._protocol, self._host, self._port, self._api_version, path), data=json.dumps(data), verify=False, auth=HTTPDigestAuth(self._user, self._password), timeout=TIMEOUT)
             #self.on = True
             if write:
                 return True
