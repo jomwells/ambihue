bd419f5c608d201f0105626ad94602729abbbdeb
Mr-Groch <grocholski@gmail.com>
2023-03-12 22:22:29 +0100

    Config entry platforms
    https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/philips_ambilight_hue/__init__.py
+++ custom_components/philips_ambilight_hue/__init__.py
@@ -49,7 +49,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
 
     entry.async_on_unload(entry.add_update_listener(async_update_entry))
 
@@ -63,7 +66,14 @@ async def async_update_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
 
